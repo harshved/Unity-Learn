36fe98961cbfc221ce3a45909c49d295f08825d2
harshved <harshved3@gmail.com>
2022-06-03 17:34:47 +0530

    Prototype 2 added

---
+++ Prototype 2/Assets/Scripts/DetectCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //On collision of animal and food destroying both
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}

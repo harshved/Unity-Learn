b3d3511e81b2695360dabd94174d453fb158cea2
harshved <harshved3@gmail.com>
2022-06-03 17:43:03 +0530

    Challenge 1 added

---
+++ Challenge 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public GameObject propeller;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propeller.transform.Rotate(speed * Time.deltaTime * Vector3.forward);
+    }
+}
